01e2e227ee2c9cc5a395a8acdc9e7792fab6548a
jude <lawsonjdutton@gmail.com>
2022-08-06 16:24:31 -0600

    add in lsp and greetings modules

--- init.lua
+++ init.lua
@@ -1,10 +1,16 @@
 -- First, reset the `require` cache, so that I can develop this sensibly
-for name, _val in pairs(package.loaded) do
+function unsetPackage()
   package.loaded[name] = nil
 end
 
+-- This can be problematic so be wary
+for name, _val in pairs(package.loaded) do
+  pcall(unsetPackage)
+end
+
 require('plugins')
+require('lsp')
 require('telescope').load_extension 'file_browser'
 require('maps')
 require('settings')
--- require('greetings')
+require('greetings')
