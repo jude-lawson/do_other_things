85cba6d2bec54f5ca4ceb015711e3292409ce476
jude <lawsonjdutton@gmail.com>
2022-08-06 16:28:28 -0600

    add some theme, LSP, and case sensitivity settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -15,6 +15,8 @@ set{
   writebackup = false,
 }                           -- no backup files (LSPs have trouble with these)
 set{number = true}          -- enable line numbers
+set{ignorecase = true}      -- case insensitive searching
+set{smartcase = true}       -- search is case insensitive, unless an uppercase letter is added
 set{signcolumn = 'number'}  -- use line numbers for the side column where signs for errors and such appear
 set{swapfile = false}       -- none of those dastardly swapfiles
 set{hlsearch = false}       -- disable highlighting search results
@@ -25,12 +27,18 @@ set{
 }                           -- set <Tab> to 2 spaces
 set{smarttab = true}        -- align tabs width at the beginning of a line
 
+-- For file completion
+set{completeopt = 'menu,menuone,noselect'}
+
 -- Additional configuration
 cmd 'syntax on'             -- enable syntax highlighting
 
 -- Colorscheme configuration
-set{background = 'dark'}      -- rose-pine will respect light or dark
-cmd 'colorscheme rose-pine'   -- set colorscheme
+-- set{background = 'dark'}      -- rose-pine will respect light or dark
+-- cmd 'colorscheme rose-pine'   -- set colorscheme
+-- vim.g.catppuccin_flavour = "latte"
+vim.g.catppuccin_flavour = "mocha"
+cmd 'colorscheme catppuccin'   -- set colorscheme
 
 -- End statement
 print('Settings loaded.')
